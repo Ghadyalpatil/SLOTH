6655185f65a7701256267021f3389df4d58a3bda
Atharva9131 <atharvaghate788@gmail.com>
2026-06-12 16:11:40 +0530

    SPECIFICATION FOR THE WEB UPDATE
    I HAVE MADE A LIST OF COMMENTS ON THE FILE STATING THE FEATURES TO ADD WHICH INCLUDE A LOT OF FEATURE BUT A FEW ARE FEATURES WHICH WE NEED OT ACT ON IMMEDIATE BASIS

--- avgcode.py
+++ avgcode.py
@@ -1,5 +1,77 @@
-print("hey guys now i know how to use github to extent where we can start with the project ")
+print("the backend specifications are given below  ")
 #what are the backend specifications 
 #target 50 specs to think on , which the website should highlight 
-#1 :- Engine needs to create notes 
-#extract text from uploaded pdf 
+#FEATURES
+
+#IMMEDIATE FEATURES TO WORK ON 
+# Smart Notes Generator
+#Previous Year Question Analyzer
+#Professor Pattern Detection
+#Personalized Explanation Engine
+#Quiz Generator
+#Study Planner
+#Progress Dashboard
+#Mind Map Generator
+#AI Doubt Solver
+####Create Your Own Feature (CYOF)###
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# #
+# 1 :- AI LEARNING FEATURES#
+# a - smart notes generator 
+# b - exam notes generator 
+# c - one page revision notes 
+# d - mental image generator 
+# e - topic breakdown engine #
+#    e.1 - cuts big topics into small chunks 
+#     e.2 - prerequisites and parts of the topic
+# f - learning style detection 
+# 
+# 2 :- Exam Related Features 
+# a - pyq analyzer
+# b - repeated question finding 
+# c - topic repeater
+# d - marks distribution according to the analyzer 
+# e - high probability topics / questions dashboard 
+# f - syllabus coverage checker 
+# 
+# 3 :- Assessment features 
+# a - quiz generator , mcq , 
+# b - mock question paper generator 
+# d - answer evaluation system 
+# e - weak area detection / more focus section out of the syllabus
+# f - #
+#
+# 3 :- future Dashboard features like teams / google 
+# 3.1 - professional dashboard for students 
+# 3.2 - assignment tracker 
+# 3.3 - deadline tracker 
+# 3.4 - exam countdown 
+# 3.5 - study planner / self planned / college recommended  
+# 3.6 - semester planner
+# 3.7 - task management dashboard
+# 
+# 4 :- personalized dashboard features for subscribed colleges/schools/institutions 
+# 4.1 - student dashboard 
+# 4.2 - teachers dashboard   
+# 4.3 - parents dashboard 
+# 4.4 - progress dashboard 
+# 4.5 - academic analytics dashboard 
+# 4.6 - subject wise topic progress tracker 
+# 4.7 - exam readiness meter - on a scale of 5 to 10 rating the student how likely is he to perform in the examination
+# #
+#
+#
+#
+#
+#
+#
+
+
+
