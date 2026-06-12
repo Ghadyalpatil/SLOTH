bc2520872fc06ae682ca0e1fe444439303e73cb8
Atharva9131 <atharvaghate788@gmail.com>
2026-06-12 14:03:06 +0530

    Update avgcode.py

--- avgcode.py
+++ avgcode.py
@@ -1,3 +1,4 @@
 print("hey guys ")
 #what are the backend specifications 
-#target 50 specs to think on , which the website should highlight 
+#target 50 specs to think on , which the website should highlight
+#1 the engine should be able to extract the text from the uploaded pdf 
