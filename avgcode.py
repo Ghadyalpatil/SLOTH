be09245a52fd02219e69b12fc2276f0e7bdffdac
Atharva9131 <atharvaghate788@gmail.com>
2026-06-12 14:10:22 +0530

    Update avgcode.py

--- avgcode.py
+++ avgcode.py
@@ -1,4 +1,3 @@
-print("hey guys ")
 print("hey guys now i know how to use github to extent where we can start with the project ")
 #what are the backend specifications 
 #target 50 specs to think on , which the website should highlight
